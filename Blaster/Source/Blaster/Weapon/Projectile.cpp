4b15d51cfd155acfc8c81696af6929805c16b895
Eu4ng <eu4ng97@gmail.com>
2023-10-01 23:40:25 +0900

    발사체 및 발사체 무기 클래스 생성

---
+++ Blaster/Source/Blaster/Weapon/Projectile.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile.h"
+
+#include "Components/BoxComponent.h"
+
+AProjectile::AProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	SetRootComponent(CollisionBox);
+	CollisionBox->SetCollisionObjectType(ECC_WorldDynamic);
+	CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CollisionBox->SetCollisionResponseToAllChannels(ECR_Ignore);
+	CollisionBox->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	CollisionBox->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
+}
+
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
